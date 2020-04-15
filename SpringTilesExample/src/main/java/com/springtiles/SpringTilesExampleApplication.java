b579de4565eb9001b62f5b382e50c32fad329e76
kiran2598 <saikiranammireddy@gmail.com>
2020-04-15 12:18:43 +0530

    First Commit

---
+++ SpringTilesExample/src/main/java/com/springtiles/SpringTilesExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springtiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTilesExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTilesExampleApplication.class, args);
+	}
+
+}
---
+++ SpringTilesExample/src/main/webapp/WEB-INF/bodyfragments/_contactus.jsp
@@ -0,0 +1,7 @@
+Contact Us: vmokshagroups
+<br>
+Address: ${address}
+<br>
+Phone: ${phone}
+<br>
+Email: ${email}
